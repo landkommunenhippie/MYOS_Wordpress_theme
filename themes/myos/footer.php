90388203b2f89239f7934004cde28a85d1e9f858
landkommunenhippie <hahn.tobias66@gmail.com>
2016-11-14 16:49:13 +0100

    minor fixes

--- themes/myos/footer.php
+++ themes/myos/footer.php
@@ -8,12 +8,12 @@ $imprint = get_page_by_title("imprint", OBJECT);
 		<div class="footer grid semi-bold">
 			<div class="row">
 				
-			<div class="col-sm-7 tablet-invisible">&#169; <?php echo date('Y'); ?>  MYOS Workshops</div>
-				<div class="col-sm-1"><a href="<?php echo get_post_permalink($faq->ID) ?>">FAQ</a></div>
-				<div class="col-sm-1"><a href="<?php echo get_post_permalink($imprint->ID) ?>">Imprint</a></div>
-				<div class="col-sm-1"><a href="<?php echo get_post_permalink($imprint->ID) ?>">Privacy</a></div>
-				<div class="col-sm-1"><a target="_blank" href="https://www.facebook.com/MakeYourOwnSign/">Facebook</a></div>
-				<div class="col-sm-1"><a href="#">#MYOS</a></div>
+			<div class="col-md-7 tablet-invisible">&#169; <?php echo date('Y'); ?>  MYOS Workshops</div>
+				<div class="col-md-1 col-sm-12"><a href="<?php echo get_post_permalink($faq->ID) ?>">FAQ</a></div>
+				<div class="col-md-1 col-sm-12"><a href="<?php echo get_post_permalink($imprint->ID) ?>">Imprint</a></div>
+				<div class="col-md-1 col-sm-12"><a href="<?php echo get_post_permalink($imprint->ID) ?>">Privacy</a></div>
+				<div class="col-md-1 col-sm-12"><a target="_blank" href="https://www.facebook.com/MakeYourOwnSign/">Facebook</a></div>
+				<div class="col-md-1 col-sm-12"><a href="#">#MYOS</a></div>
 			</div>
 			<div class="row tablet-visible">
 				<div class="col-md-12">&#169; <?php echo date('Y'); ?>  MYOS Workshops</div>
